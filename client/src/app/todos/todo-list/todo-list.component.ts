b22d8353612c188123e0f754ba2d8e0ccb299e16
Erik <eriktr22@gmail.com>
2020-02-20 14:16:19 -0600

    Fix errors in html and set up framework for filtering
    Just needs the todo.ts file from the other branch

--- client/src/app/todos/todo-list/todo-list.component.ts
+++ client/src/app/todos/todo-list/todo-list.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Todo, TodoRole } from './../todo';
+import { Todo } from './../todo';
 import { TodoService } from './../todo.service';
 
 @Component({
@@ -8,8 +8,28 @@ import { TodoService } from './../todo.service';
   styleUrls: ['./todo-list.component.scss']
 })
 export class TodoListComponent implements OnInit {
+  // These are public so that tests can reference them (.spec.ts)
+  public serverFilteredTodos: Todo[];
+  public filteredTodos: Todo[];
 
-  constructor() { }
+  //To be done in angular
+  public todoOwner: string;
+  public todoContains: string;
+  public limit: number;
+
+  //To be done on the server
+  public todoStatus: boolean;
+  public todoRole: string;
+  public orderBy: string;
+
+
+  constructor( private todoService: TodoService ) {
+
+  }
+
+  public updateFilter() {
+
+  }
 
   ngOnInit(): void {
   }
